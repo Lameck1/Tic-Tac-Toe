cce05ac288321c528b16f734b74f37c2ad5f24b4
smcommits <sm.anas@outlook.com>
2020-11-05 20:11:05 +0530

    Add methods: validate, display_details, and play to game class
    - Validate methods validates the user input for a move
    - Display details display player details
    - Play intializes one instance of a game

--- lib/game.rb
+++ lib/game.rb
@@ -18,5 +18,42 @@ class Game
         print "#{current_player.name}, please pick your position:  " 
         gets.chomp 
       end   
+  def validate_input(board, position)
+    until board.valid_move?(position)
+      puts "InputError: Please enter select the cell between 1 and 9: " unless position.to_i.between?(1, 9)
+      puts "InputError: Please select cell that is not taken (refer to cell labels)" if board.position_taken?(position)
+      print "Please enter the value again: "
+      position = gets.chomp
+    end
+    position
+  end
+
+  def display_detail(player_a, player_b)
+    system ('clear')
+    puts 'This is a tic-tac-toe game'
+    print "Player 1: #{player_a.name} | Player 2: #{player_b.name}"
+    puts " "
+  end
+
+  def play(board, current_player, player_a, player_b)
+    loop do
+      display_detail(player_a, player_b)
+      board.draw
+      position = get_user_input(current_player)
+      validated_position = validate_input(board, position)
+      board.update(validated_position, current_player)
+      system('clear') 
+      if board.win?
+        board.draw
+        puts "#{current_player.name} is the WINNER!!!!".colorize(:yellow)
+        break
+      elsif board.tied?
+        board.draw
+        puts"Looks like it is a Tie!!".colorize(:green)
+        break
+      end
+      current_player = board.switch_player(player_a, player_b, current_player) 
+    end
+  end
 
 end

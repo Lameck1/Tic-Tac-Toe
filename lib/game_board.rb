64db1d739acd7b2b595ebfb09fbd8205a512d30f
Lameck1 <lameck72@gmail.com>
2020-11-05 17:19:51 +0300

    Add draw, valid_move? and position_taken? methods
    draw method, draws the board
    valid_move validates players move on the board
    position_taken prevents re-usage of board cells

--- lib/game_board.rb
+++ lib/game_board.rb
@@ -1,3 +1,4 @@
+require_relative './colorize-0.8.1/lib/colorize'
 class Board
   attr_accessor :board
 
@@ -5,12 +6,20 @@ class Board
     @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   end
 
-  def valid_move?(_position)
-    nil
+  def draw
+    puts "|#{@board[0]} | #{@board[1]} | #{@board[2]}|"
+    puts '---+--------'
+    puts "|#{@board[3]} | #{@board[4]} | #{@board[5]}|"
+    puts '---+--------'
+    puts "|#{@board[6]} | #{@board[7]} | #{@board[8]}|"
   end
 
-  def position_taken?(_position)
-    nil
+  def valid_move?(position)
+    position.to_i.between?(1, 9) && !position_taken?(position)
+  end
+
+  def position_taken?(position)
+    @board[position.to_i - 1].is_a?(String)
   end
 
   def update(_position, _player_turn, _player_a_c, _player_b_c)

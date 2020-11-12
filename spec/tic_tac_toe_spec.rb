7f3f9b460aedc4d21f345a16884d73101481ff9d
Lameck1 <lameck72@gmail.com>
2020-11-12 13:33:31 +0300

    Add tests: #update, #switch_players, #win?, #tied?
    - #update
    	- updates the board with current_player's character
    - #switch_players
    	- sets current_player to next player
    - #win?
    	- returns true if a current_player's input completes a win
    	  combination
    	- returns false if a current_player's input won't complete a win
    	  combination
    - #tied?
    	- returns true if all cell values are strings but with no win
    	  combination
    	- returns false if all cell values are strings but with a win
    	  combination

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -1,6 +1,6 @@
-require '../lib/game_player'
-require '../lib/game_board'
-require '../lib/game'
+require './lib/game_player'
+require './lib/game_board'
+require './lib/game'
 
 describe Player do
   let(:player) { Player.new('Misori', 'X') }
@@ -60,6 +60,64 @@ describe Board do
     it 'returns false if player selects a cell with an Integer value' do
       new_position = '6'
       expect(subject.position_taken?(new_position)).not_to eq(true)
+    end
+  end
+
+  describe '#update' do
+    it "updates the board with current_player's character" do
+      subject.update(position, current_player)
+      expect(subject.board).to match_array([1, 2, 3, 4, 'X', 6, 7, 8, 9])
+    end
+  end
+
+  describe '#switch_players' do
+    it 'sets current_player to next player' do
+      next_player = subject.switch_player(player1, player2, current_player)
+      expect(next_player).to eq player2
+    end
+  end
+
+  describe '#win?' do
+    it "returns true if a current_player's input completes a win combination" do
+      subject.update('1', current_player)
+      subject.update('5', current_player)
+      subject.update('9', current_player)
+      expect(subject.win?).to eq(true)
+    end
+
+    it "returns false if a current_player's input won't complete a win combination" do
+      subject.update('1', current_player)
+      subject.update('5', current_player)
+      subject.update('4', current_player)
+      expect(subject.win?).not_to eq(true)
+    end
+  end
+
+  describe '#tied?' do
+    it 'returns true if all cell values are strings but with no win combination' do
+      subject.update('1', player1)
+      subject.update('9', player2)
+      subject.update('4', player1)
+      subject.update('7', player2)
+      subject.update('5', player1)
+      subject.update('6', player2)
+      subject.update('3', player1)
+      subject.update('2', player2)
+      subject.update('8', player1)
+      expect(subject.tied?).to eq(true)
+    end
+
+    it 'returns false if all cell values are strings but with a win combination' do
+      subject.update('1', player1)
+      subject.update('2', player2)
+      subject.update('5', player1)
+      subject.update('7', player2)
+      subject.update('9', player1)
+      subject.update('3', player2)
+      subject.update('2', player1)
+      subject.update('8', player2)
+      subject.update('4', player1)
+      expect(subject.tied?).not_to eq(true)
     end
   end
 end
